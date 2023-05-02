3dcfeec32f1a5ee5d1a04c4790a76427b96c42b2
Ajay Prakash Nair <ajaynair710@gmail.com>
2023-05-02 21:50:53 +0200

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import Hero from './components/Hero'
 import Demo from './components/Demo'
+import './App.css'
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <main>
+      <div className='main'>
+        <div className='gradient'>
+        </div>
+      </div>
+
+      <div className='app'>
+        <Hero />
+        <Demo />
+      </div>
+    </main>
   )
 }
 
